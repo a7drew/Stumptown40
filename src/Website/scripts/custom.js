205e3d3c9e98d47ab384448211897b29397b82ac
Andrew Hay <andrewcameronhay@hotmail.com>
2012-04-23 11:10:34 -0700

    Moved todo list to github issues.

--- src/Website/scripts/custom.js
+++ src/Website/scripts/custom.js
@@ -1,13 +1,4 @@
-﻿/* 
-TODO: 
-	Style the site
-	Add racer slideshow to play before the race
-	Add racers to loser bracket
-	Randomize the initial bracket	
-	Display the average race time, or average racers per (some unit of time)
-*/
-
-// MODELS ======================================================================
+﻿// MODELS ======================================================================
 
 var Bracket = Backbone.Model.extend({});
 
@@ -18,9 +9,9 @@ var Brackets = Backbone.Collection.extend({
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
 	},
-    url: function() {
-    	return "http://stumptown40.cloudapp.net/brackets";
-    }
+	url: function() {
+		return "http://stumptown40.cloudapp.net/brackets";
+	}
 });
 
 var brackets = new Brackets();
@@ -30,11 +21,11 @@ var brackets = new Brackets();
 var Round = Backbone.Model.extend({});
 
 var Rounds = Backbone.Collection.extend({
-    model: Round,
-    url: function () {
-        return "http://stumptown40.cloudapp.net/rounds/index/" + this.BracketId;
-    },
-    sync: function(method, model, options) {  
+	model: Round,
+	url: function () {
+		return "http://stumptown40.cloudapp.net/rounds/index/" + this.BracketId;
+	},
+	sync: function(method, model, options) {  
 		options.timeout = 10000;  
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
@@ -45,10 +36,10 @@ var rounds = new Rounds();
 // -----------------------------------------------------------------------------
 
 var MatchHeader = Backbone.Model.extend({
-    url: function () {
-        return "http://stumptown40.cloudapp.net/matches/header/" + this.RoundId;
-    },
-    sync: function(method, model, options) {  
+	url: function () {
+		return "http://stumptown40.cloudapp.net/matches/header/" + this.RoundId;
+	},
+	sync: function(method, model, options) {  
 		options.timeout = 10000;  
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
@@ -62,12 +53,12 @@ var matchHeader = new MatchHeader();
 var Match = Backbone.Model.extend({});
 
 var Matches = Backbone.Collection.extend({
-    model: Match,
-    url: function ()
-    {
-        return "http://stumptown40.cloudapp.net/matches/index/" + this.RoundId;
-    },
-    sync: function(method, model, options) {  
+	model: Match,
+	url: function ()
+	{
+		return "http://stumptown40.cloudapp.net/matches/index/" + this.RoundId;
+	},
+	sync: function(method, model, options) {  
 		options.timeout = 10000;  
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
@@ -79,10 +70,10 @@ var matches = new Matches();
 // -----------------------------------------------------------------------------
 
 var Race = Backbone.Model.extend({
-    url: function () {
-        return "http://stumptown40.cloudapp.net/matches/detail/" + this.MatchId;
-    },
-    sync: function(method, model, options) {  
+	url: function () {
+		return "http://stumptown40.cloudapp.net/matches/detail/" + this.MatchId;
+	},
+	sync: function(method, model, options) {  
 		options.timeout = 10000;  
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
@@ -96,9 +87,9 @@ var race = new Race();
 var Person = Backbone.Model.extend({});
 
 var People = Backbone.Collection.extend({
-    model: Person,
+	model: Person,
 	url: "http://stumptown40.cloudapp.net/racers",
-    sync: function(method, model, options) {  
+	sync: function(method, model, options) {  
 		options.timeout = 10000;  
 		options.dataType = "jsonp";  
 		return Backbone.sync(method, model, options);  
@@ -117,7 +108,7 @@ function PostWinner(matchId, winningRacerId, losingRacerId) {
 		type: 'post',
 		url: 'http://stumptown40.cloudapp.net/matches/update',
 		data: { matchId: matchId, winningRacerId:winningRacerId, losingRacerId:losingRacerId }
-    });
+	});
 
 }
 
@@ -146,76 +137,76 @@ var BracketView = Backbone.View.extend({
 // -----------------------------------------------------------------------------
 
 var RoundView = Backbone.View.extend({
-    tagName: "div",
-    template: _.template($('#round-template').html()),
-    render: function () {
-        this.$el.html(this.template({ list: this.collection.toJSON() }));
-        return this;
-    }
+	tagName: "div",
+	template: _.template($('#round-template').html()),
+	render: function () {
+		this.$el.html(this.template({ list: this.collection.toJSON() }));
+		return this;
+	}
 });
 
 // -----------------------------------------------------------------------------
 
 var MatchView = Backbone.View.extend({
-    tagName: "div",
-    template: _.template($('#match-template').html()),
-    render: function () {
-        this.$el.html(this.template({ list: this.collection.toJSON() }));
-        return this;
-    }
+	tagName: "div",
+	template: _.template($('#match-template').html()),
+	render: function () {
+		this.$el.html(this.template({ list: this.collection.toJSON() }));
+		return this;
+	}
 });
 
 // -----------------------------------------------------------------------------
 
 var MatchHeaderView = Backbone.View.extend({
-    tagName: "div",
-    template: _.template($('#match-header-template').html()),
-    initialize: function () {
-        _.bindAll(this, 'render');
-    },
-    render: function () {
-        this.$el.html(this.template({ model: this.model.toJSON() }));
-        return this;
-    }
+	tagName: "div",
+	template: _.template($('#match-header-template').html()),
+	initialize: function () {
+		_.bindAll(this, 'render');
+	},
+	render: function () {
+		this.$el.html(this.template({ model: this.model.toJSON() }));
+		return this;
+	}
 });
 
 // -----------------------------------------------------------------------------
 
 var RaceView = Backbone.View.extend({
-    tagName: "div",
-    template: _.template($('#race-template').html()),
-    initialize: function () {
-        _.bindAll(this, 'render');
-    },
-    events: {
-    	"click #racer1Won" : "racer1Won",
-    	"click #racer2Won" : "racer2Won"
-    },
-    racer1Won : function () {
+	tagName: "div",
+	template: _.template($('#race-template').html()),
+	initialize: function () {
+		_.bindAll(this, 'render');
+	},
+	events: {
+		"click #racer1Won" : "racer1Won",
+		"click #racer2Won" : "racer2Won"
+	},
+	racer1Won : function () {
 
-    	var matchId = $('#racer1Won').attr('data-matchId');
-    	var winningRacerId = $('#racer1Won').attr('data-racerId');
-    	var losingRacerId = $('#racer2Won').attr('data-racerId');
+		var matchId = $('#racer1Won').attr('data-matchId');
+		var winningRacerId = $('#racer1Won').attr('data-racerId');
+		var losingRacerId = $('#racer2Won').attr('data-racerId');
 
 		PostWinner(matchId, winningRacerId, losingRacerId);
 
-    	alert('show winner here');
-
-    },
-    racer2Won : function () {
-    	
-		var matchId = $('#racer2Won').attr('data-matchId');
-    	var winningRacerId = $('#racer2Won').attr('data-racerId');
-    	var losingRacerId = $('#racer1Won').attr('data-racerId');
-
-    	PostWinner(matchId, winningRacerId, losingRacerId);
-    	
 		alert('show winner here');
-    },
-    render: function () {
-        this.$el.html(this.template({ model: this.model.toJSON() }));
-        return this;
-    }
+
+	},
+	racer2Won : function () {
+		
+		var matchId = $('#racer2Won').attr('data-matchId');
+		var winningRacerId = $('#racer2Won').attr('data-racerId');
+		var losingRacerId = $('#racer1Won').attr('data-racerId');
+
+		PostWinner(matchId, winningRacerId, losingRacerId);
+		
+		alert('show winner here');
+	},
+	render: function () {
+		this.$el.html(this.template({ model: this.model.toJSON() }));
+		return this;
+	}
 });
 
 // -----------------------------------------------------------------------------
@@ -254,85 +245,85 @@ var FakeSponsorView = Backbone.View.extend({
 // ROUTER =====================================================================
 
 var App = Backbone.Router.extend({
-    routes: {
-        'home': 'home',
-        'brackets': 'bracketList',
-        'brackets/:id': 'roundList',
-        'rounds/:id': 'matchList',
-        'matches/:id': 'matchItem',
-        'matches/:id/*update': 'matchItem',
-        'people': 'personList',
-        'sponsors': 'sponsorsList',
-        'fakesponsors': 'fakeSponsorsList'
-    },
-    home: function () {
-        var view = new HomeView();
-        $("#main").html(view.render().el);
-    },
-    bracketList: function () {
+	routes: {
+		'home': 'home',
+		'brackets': 'bracketList',
+		'brackets/:id': 'roundList',
+		'rounds/:id': 'matchList',
+		'matches/:id': 'matchItem',
+		'matches/:id/*update': 'matchItem',
+		'people': 'personList',
+		'sponsors': 'sponsorsList',
+		'fakesponsors': 'fakeSponsorsList'
+	},
+	home: function () {
+		var view = new HomeView();
+		$("#main").html(view.render().el);
+	},
+	bracketList: function () {
 		brackets.fetch({
 			success: function (model, response) {
 				var view = new BracketView({ collection: brackets });
 				$("#main").html(view.render().el);
 			}
 		});
-    },
-    roundList: function (id) {
-        rounds.BracketId = id;
-        rounds.fetch({
-            success: function (model, response) {
-                var view = new RoundView({ collection: rounds });
-                $('#main').html(view.render().el);
-            }
-        });
-    },
-    matchList: function (id) {
+	},
+	roundList: function (id) {
+		rounds.BracketId = id;
+		rounds.fetch({
+			success: function (model, response) {
+				var view = new RoundView({ collection: rounds });
+				$('#main').html(view.render().el);
+			}
+		});
+	},
+	matchList: function (id) {
 
-    	$('#main').html('<div id="header"></div><div id="detail"></div>');
+		$('#main').html('<div id="header"></div><div id="detail"></div>');
 
 		matchHeader.RoundId = id;
-        matchHeader.fetch({
-            success: function (model, response) {
-                var view = new MatchHeaderView({ model: matchHeader });
-                $('#header').append(view.render().el);
-            }
-        });
+		matchHeader.fetch({
+			success: function (model, response) {
+				var view = new MatchHeaderView({ model: matchHeader });
+				$('#header').append(view.render().el);
+			}
+		});
 
 		matches.RoundId = id;
-        matches.fetch({
-            success: function (model, response) {
-                var view = new MatchView({ collection: matches });
-                var $list = $(view.render().el);
-            	$('#detail').append($list);
-            }
-        });
+		matches.fetch({
+			success: function (model, response) {
+				var view = new MatchView({ collection: matches });
+				var $list = $(view.render().el);
+				$('#detail').append($list);
+			}
+		});
 		
-    },
-    matchItem: function (id) {
-        race.MatchId = id;
-        race.fetch({
-            success: function (model, response) {
-                var view = new RaceView({ model: race });
-                $('#main').html(view.render().el);
-            }
-        });
-    },
-    personList: function () {
-        people.fetch({
-            success: function (model, response) {
-                var view = new PeopleView({ collection: people });
-                $('#main').html(view.render().el);
-            }
-        });
-    },
-    sponsorsList: function () {
-        var view = new SponsorView();
-        $('#main').html(view.render().el);
-    },
-    fakeSponsorsList: function () {
-        var view = new FakeSponsorView();
-        $('#main').html(view.render().el);
-    }
+	},
+	matchItem: function (id) {
+		race.MatchId = id;
+		race.fetch({
+			success: function (model, response) {
+				var view = new RaceView({ model: race });
+				$('#main').html(view.render().el);
+			}
+		});
+	},
+	personList: function () {
+		people.fetch({
+			success: function (model, response) {
+				var view = new PeopleView({ collection: people });
+				$('#main').html(view.render().el);
+			}
+		});
+	},
+	sponsorsList: function () {
+		var view = new SponsorView();
+		$('#main').html(view.render().el);
+	},
+	fakeSponsorsList: function () {
+		var view = new FakeSponsorView();
+		$('#main').html(view.render().el);
+	}
 });
 
 // =============================================================================
