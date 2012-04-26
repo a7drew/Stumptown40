d0bc8fd30c9419b97084a30ef30e7a32ed5da6fb
Andrew Hay <andrewcameronhay@hotmail.com>
2012-04-26 14:53:56 -0700

    added twitter client

---
+++ src/Website/Controllers/TweetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LinqToTwitter;
+using Website.Infrastructure;
+using Website.Models;
+
+namespace Website.Controllers
+{
+	public class TwitterController : Controller
+	{
+		public ActionResult Index(string term)
+		{
+			var twitterCtx = new TwitterContext();
+
+            var queryResults =
+                from search in twitterCtx.Search
+                where search.Type == SearchType.Search &&
+                      search.Query == term
+                select search;
+
+			var model = queryResults.Single().Results;
+
+			return new JsonpResult
+			{
+				Data = model, 
+				JsonRequestBehavior = JsonRequestBehavior.AllowGet
+			};
+		}
+	}
+}
