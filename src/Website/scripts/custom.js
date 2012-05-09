a88f0e9586e39707dabdc8364336bc39d478dd77
Andrew Hay <andrewcameronhay@hotmail.com>
2012-05-09 16:12:31 -0700

    added back navigation to views

--- src/Website/scripts/custom.js
+++ src/Website/scripts/custom.js
@@ -5,7 +5,7 @@
 //	http://stumptown40.cloudapp.net
 //
 
-var webSvcUrl = 'http://localhost:28555';
+var webSvcUrl = 'http://stumptown40.cloudapp.net';
 
 var Bracket = Backbone.Model.extend({});
 
@@ -111,14 +111,15 @@ var people = new People();
 
 function PostWinner(matchId, winningRacerId, losingRacerId) {
 
-	$.ajax({
-		type: 'post',
-		url: webSvcUrl + '/matches/update',
-		data: { matchId: matchId, winningRacerId:winningRacerId, losingRacerId:losingRacerId },
-		success: function () {
-			$('#raceTemplate').append('<input id="hiddenPostedWinner" type="hidden" value="' + winningRacerId + '" />');
-		}
-	});
+    $.ajax({
+        type: 'post',
+        url: webSvcUrl + '/matches/update',
+        data: { matchId: matchId, winningRacerId: winningRacerId, losingRacerId: losingRacerId },
+        success: function ()
+        {
+            $('#raceTemplate').append('<input id="hiddenPostedWinner" type="hidden" value="' + winningRacerId + '" />');
+        }
+    });
 
 }
 
@@ -189,47 +190,49 @@ var MatchHeaderView = Backbone.View.extend({
 // -----------------------------------------------------------------------------
 
 var RaceView = Backbone.View.extend({
-	tagName: "div",
-	template: _.template($('#race-template').html()),
-	initialize: function () {
-		_.bindAll(this, 'render');
-	},
-	events: {
-		"click #racer1Won" : "racer1Won",
-		"click #racer2Won" : "racer2Won"
-	},
-	racer1Won : function () {
+    tagName: "div",
+    template: _.template($('#race-template').html()),
+    initialize: function ()
+    {
+        _.bindAll(this, 'render');
+    },
+    events: {
+        "click #racer1Won": "racer1Won",
+        "click #racer2Won": "racer2Won"
+    },
+    racer1Won: function ()
+    {
+        var matchId = $('#racer1Won').attr('data-matchId');
+        var winningRacerId = $('#racer1Won').attr('data-racerId');
+        var losingRacerId = $('#racer2Won').attr('data-racerId');
 
-		var matchId = $('#racer1Won').attr('data-matchId');
-		var winningRacerId = $('#racer1Won').attr('data-racerId');
-		var losingRacerId = $('#racer2Won').attr('data-racerId');
+        PostWinner(matchId, winningRacerId, losingRacerId);
 
-		PostWinner(matchId, winningRacerId, losingRacerId);
-		this.showWinner(winningRacerId, $(racer1Won).text());
-	},
-	racer2Won : function () {
-		
-		var matchId = $('#racer2Won').attr('data-matchId');
-		var winningRacerId = $('#racer2Won').attr('data-racerId');
-		var losingRacerId = $('#racer1Won').attr('data-racerId');
+        $('div.racers div.racer1 div.card').addClass('winner');
+        $('div.racers div.racer2 div.card').removeClass('winner');
+    },
+    racer2Won: function ()
+    {
+        var matchId = $('#racer2Won').attr('data-matchId');
+        var winningRacerId = $('#racer2Won').attr('data-racerId');
+        var losingRacerId = $('#racer1Won').attr('data-racerId');
 
-		PostWinner(matchId, winningRacerId, losingRacerId);
-		this.showWinner(winningRacerId, $(racer2Won).text());
-	},
-	render: function () {
+        PostWinner(matchId, winningRacerId, losingRacerId);
 
-		var m = this.model.toJSON();
-		this.$el.html(this.template({ model: m }));
-		if(m.Winner.RacerId === 0) {
-			this.$el.find('#winner').hide();
-		}
-		return this;
-	},
-	showWinner: function(racerId, racerName) {
-		$('#winner span').html('#' + racerId + ' ' + racerName);
-		$('#winner').show();
-	
-	}
+        $('div.racers div.racer2 div.card').addClass('winner');
+        $('div.racers div.racer1 div.card').removeClass('winner');
+    },
+    render: function ()
+    {
+
+        var m = this.model.toJSON();
+        this.$el.html(this.template({ model: m }));
+        if (m.Winner.RacerId === 0)
+        {
+            this.$el.find('#winner').hide();
+        }
+        return this;
+    }
 });
 
 // -----------------------------------------------------------------------------
@@ -268,158 +271,192 @@ var FakeSponsorView = Backbone.View.extend({
 // ROUTER =====================================================================
 
 var App = Backbone.Router.extend({
-	routes: {
-		'home': 'home',
-		'brackets': 'bracketList',
-		'brackets/:id': 'roundList',
-		'rounds/:id': 'matchList',
-		'matches/:id': 'matchItem',
-		'matches/:id/*update': 'matchItem',
-		'people': 'personList',
-		'sponsors': 'sponsorsList',
-		'fakesponsors': 'fakeSponsorsList'
-	},
-	home: function () {
-		var view = new HomeView();
-		$("#main").html(view.render().el);
-			var mytoken = '25329.f59def8.1a9eb7a77f2b46eeb5cec55fa3457d6d',
+    routes: {
+        'home': 'home',
+        'brackets': 'bracketList',
+        'brackets/:id': 'roundList',
+        'rounds/:id/m:matchId': 'matchList',
+        'rounds/:id': 'matchList',
+        'matches/:id': 'matchItem',
+        'matches/:id/*update': 'matchItem',
+        'people': 'personList',
+        'sponsors': 'sponsorsList',
+        'fakesponsors': 'fakeSponsorsList'
+    },
+    home: function ()
+    {
+        var view = new HomeView();
+        $("#main").html(view.render().el);
+        var mytoken = '25329.f59def8.1a9eb7a77f2b46eeb5cec55fa3457d6d',
 				pullcount = 200;
-			function instagram() {
-				var token = mytoken,
+        function instagram()
+        {
+            var token = mytoken,
 					count = pullcount;
-				    $.ajax({
-				        type: "GET",
-				        dataType: "jsonp",
-				 		async: true,
-				        cache: false,
-				        url: 'https://api.instagram.com/v1/users/self/media/recent?access_token='+token+'&count='+count+'',
-				        success: function(data) {
-				        	$.each(data.data, function(index, value){
-								var id = this.id,
+            $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                async: true,
+                cache: false,
+                url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + count + '',
+                success: function (data)
+                {
+                    $.each(data.data, function (index, value)
+                    {
+                        var id = this.id,
 								 	seconds = index * 2;
-									$('#instagram').addClass("animate");
-									$('#instagram.animate').append('<li><img style="-moz-animation-name: big; -moz-animation-delay:'+seconds+'s; -webkit-animation-name: big; -webkit-animation-delay:'+seconds+'s;" data-id="'+id+'" src="'+this.images.standard_resolution.url+'" alt="" /></li>');
-					         });   
-				        },
-						complete: function(){
-							$('#instagram li:last-child img').on('webkitAnimationEnd oAnimationEnd msAnimationEnd animationend', function() {
-								$("#instagram li").each(function(){
-										$(this).children("img").css({
-											"-moz-animation-name":  "off",
-											"-webkit-animation-name":  "off"	
-										});
-										//Not working for webkit for some reason. Alas, we will use Firefox!
-										setTimeout(function(){
-											$("#instagram li").each(function(){
-												$(this).children("img").css({
-													"-moz-animation-name":  "big",
-													"-webkit-animation-name":  "big"
-												});
-											});
-										}, 10);
-								});	
-							});
-						}
-				    });
-			}
-			function newphoto() {
-				var token = mytoken,
+                        $('#instagram').addClass("animate");
+                        $('#instagram.animate').append('<li><img style="-moz-animation-name: big; -moz-animation-delay:' + seconds + 's; -webkit-animation-name: big; -webkit-animation-delay:' + seconds + 's;" data-id="' + id + '" src="' + this.images.standard_resolution.url + '" alt="" /></li>');
+                    });
+                },
+                complete: function ()
+                {
+                    $('#instagram li:last-child img').on('webkitAnimationEnd oAnimationEnd msAnimationEnd animationend', function ()
+                    {
+                        $("#instagram li").each(function ()
+                        {
+                            $(this).children("img").css({
+                                "-moz-animation-name": "off",
+                                "-webkit-animation-name": "off"
+                            });
+                            //Not working for webkit for some reason. Alas, we will use Firefox!
+                            setTimeout(function ()
+                            {
+                                $("#instagram li").each(function ()
+                                {
+                                    $(this).children("img").css({
+                                        "-moz-animation-name": "big",
+                                        "-webkit-animation-name": "big"
+                                    });
+                                });
+                            }, 10);
+                        });
+                    });
+                }
+            });
+        }
+        function newphoto()
+        {
+            var token = mytoken,
 					count = pullcount;
-				$.ajax({
-				        type: "GET",
-				        dataType: "jsonp",
-				 		async: true,
-				        cache: false,
-				        url: 'https://api.instagram.com/v1/users/self/media/recent?access_token='+token+'&count='+count+'',
-						success: function(data) {
-							var id = data.data[0].id;
-							var photoID1 = data.data[0].id;
-							var photoID2 = $('#instagram img:nth-child(1)').attr('data-id');							
-							if(photoID1 != photoID2) {
-								$('#instagram').prepend('<li><img  style="-moz-animation-name: big; -moz-animation-delay:2s; -webkit-animation-name: big; -webkit-animation-delay:2s;" data-id="'+id+'" src="'+data.data[0].images.standard_resolution.url+'" alt="" /></li>');
-								$("#instagram li").each(function(index){
-									var seconds = index * 2;
-									$(this).children('img').css({
-										"-moz-animation-delay": seconds+"s",
-										"-webkit-animation-delay": seconds+"s"
-									});
-								});
-							}
-						}
-				});
-			}	 
-		instagram();
-		(function poll(){
-				setTimeout(function(){
-					newphoto();
-					poll();
-				}, 5000); //pull for new photos every 5 secs. allowed up to 5000 requests hour
-		})();
-	},
-	bracketList: function () {
-		brackets.fetch({
-			success: function (model, response) {
-				var view = new BracketView({ collection: brackets });
-				$("#main").html(view.render().el);
-			}
-		});
-	},
-	roundList: function (id) {
-		rounds.BracketId = id;
-		rounds.fetch({
-			success: function (model, response) {
-				var view = new RoundView({ collection: rounds });
-				$('#main').html(view.render().el);
-			}
-		});
-	},
-	matchList: function (id) {
+            $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                async: true,
+                cache: false,
+                url: 'https://api.instagram.com/v1/users/self/media/recent?access_token=' + token + '&count=' + count + '',
+                success: function (data)
+                {
+                    var id = data.data[0].id;
+                    var photoID1 = data.data[0].id;
+                    var photoID2 = $('#instagram img:nth-child(1)').attr('data-id');
+                    if (photoID1 != photoID2)
+                    {
+                        $('#instagram').prepend('<li><img  style="-moz-animation-name: big; -moz-animation-delay:2s; -webkit-animation-name: big; -webkit-animation-delay:2s;" data-id="' + id + '" src="' + data.data[0].images.standard_resolution.url + '" alt="" /></li>');
+                        $("#instagram li").each(function (index)
+                        {
+                            var seconds = index * 2;
+                            $(this).children('img').css({
+                                "-moz-animation-delay": seconds + "s",
+                                "-webkit-animation-delay": seconds + "s"
+                            });
+                        });
+                    }
+                }
+            });
+        }
+        instagram();
+        (function poll()
+        {
+            setTimeout(function ()
+            {
+                newphoto();
+                poll();
+            }, 5000); //pull for new photos every 5 secs. allowed up to 5000 requests hour
+        })();
+    },
+    bracketList: function ()
+    {
+        brackets.fetch({
+            success: function (model, response)
+            {
+                var view = new BracketView({ collection: brackets });
+                $("#main").html(view.render().el);
+            }
+        });
+    },
+    roundList: function (id, matchId)
+    {
+        rounds.BracketId = id;
+        rounds.fetch({
+            success: function (model, response)
+            {
+                var view = new RoundView({ collection: rounds });
+                $('#main').html(view.render().el);
+            }
+        });
+    },
+    matchList: function (id, matchId)
+    {
+        $('#main').html('<div id="header" class="matchListHeader"></div><div id="detail"></div>');
 
-		$('#main').html('<div id="header" class="matchListHeader"></div><div id="detail"></div>');
+        matchHeader.RoundId = id;
+        matchHeader.fetch({
+            success: function (model, response)
+            {
+                var view = new MatchHeaderView({ model: matchHeader });
+                $('#header').append(view.render().el);
+            }
+        });
 
-		matchHeader.RoundId = id;
-		matchHeader.fetch({
-			success: function (model, response) {
-				var view = new MatchHeaderView({ model: matchHeader });
-				$('#header').append(view.render().el);
-			}
-		});
+        matches.RoundId = id;
+        matches.fetch({
+            success: function (model, response)
+            {
+                var view = new MatchView({ collection: matches });
+                var $list = $(view.render().el);
+                $('#detail').append($list);
 
-		matches.RoundId = id;
-		matches.fetch({
-			success: function (model, response) {
-				var view = new MatchView({ collection: matches });
-				var $list = $(view.render().el);
-				$('#detail').append($list);
-			}
-		});
-		
-	},
-	matchItem: function (id) {
-		race.MatchId = id;
-		race.fetch({
-			success: function (model, response) {
-				var view = new RaceView({ model: race });
-				$('#main').html(view.render().el);
-			}
-		});
-	},
-	personList: function () {
-		people.fetch({
-			success: function (model, response) {
-				var view = new PeopleView({ collection: people });
-				$('#main').html(view.render().el);
-			}
-		});
-	},
-	sponsorsList: function () {
-		var view = new SponsorView();
-		$('#main').html(view.render().el).parents("body");
-	},
-	fakeSponsorsList: function () {
-		var view = new FakeSponsorView();
-		$('#main').html(view.render().el);
-	}
+                if (matchId)
+                {
+                    var selector = 'a[href="#matches/' + matchId + '"]';
+                    $(selector)[0].scrollIntoView();
+                }
+            }
+        });
+
+    },
+    matchItem: function (id)
+    {
+        race.MatchId = id;
+        race.fetch({
+            success: function (model, response)
+            {
+                var view = new RaceView({ model: race });
+                $('#main').html(view.render().el);
+            }
+        });
+    },
+    personList: function ()
+    {
+        people.fetch({
+            success: function (model, response)
+            {
+                var view = new PeopleView({ collection: people });
+                $('#main').html(view.render().el);
+            }
+        });
+    },
+    sponsorsList: function ()
+    {
+        var view = new SponsorView();
+        $('#main').html(view.render().el).parents("body");
+    },
+    fakeSponsorsList: function ()
+    {
+        var view = new FakeSponsorView();
+        $('#main').html(view.render().el);
+    }
 });
 
 // =============================================================================
