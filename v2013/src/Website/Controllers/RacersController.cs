688f9cddd476f6df1a88247bca70a3e00be2ef34
Andrew Hay <andrewcameronhay@hotmail.com>
2013-02-28 14:27:40 -0800

    added RacersController and stubbed out the one api call made by public.html

---
+++ v2013/src/Website/Controllers/RacersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class RacersController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Racer> Get()
+        {
+            return new List<Racer>
+                {
+                    new Racer{ RacerId = 1, Name="Alice Alamacher", Organization = "Alice's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car001.jpg"},
+                    new Racer{ RacerId = 2, Name="Bob Beach", Organization = "Bob's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car002.jpg"},
+                    new Racer{ RacerId = 3, Name="Charlie Caps", Organization = "Charlie's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car003.jpg"},
+                    new Racer{ RacerId = 4, Name="Delbert Dewars", Organization = "Delbert's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car004.jpg"},
+                    new Racer{ RacerId = 5, Name="Eddie Eggplant", Organization = "Eddie's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car005.jpg"},
+                };
+        }
+    }
+}
---
+++ v2013/src/Website/Models/Racer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Racer
+    {
+        public int RacerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Organization { get; set; }
+
+        public string CarUrl { get; set; }
+    }
+}
