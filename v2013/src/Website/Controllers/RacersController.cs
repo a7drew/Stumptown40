53b4fd6f51dda73c630fc23e42a71047de71dddf
Andrew Hay <andrewcameronhay@hotmail.com>
2013-03-01 16:20:53 -0800

    working on webapi interface

--- v2013/src/Website/Controllers/RacersController.cs
+++ v2013/src/Website/Controllers/RacersController.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Dapper;
 using Website.Models;
 
 namespace Website.Controllers
 {
     public class RacersController : ApiController
     {
-        // GET api/values
+        // GET api/racers
         public IEnumerable<Racer> Get()
         {
             return new List<Racer>
@@ -21,6 +21,71 @@ namespace Website.Controllers
                     new Racer{ RacerId = 4, Name="Delbert Dewars", Organization = "Delbert's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car004.jpg"},
                     new Racer{ RacerId = 5, Name="Eddie Eggplant", Organization = "Eddie's House", CarUrl = "https://stumptown40.blob.core.windows.net/cars/car005.jpg"},
                 };
+        }
+    }
+
+    public class Racers2Controller : ApiController
+    {
+        private const string Cnn = @"Server=.\sqlexpress;Database=Stumptown40;Trusted_Connection=True;";
+
+        public IEnumerable<Racer> Get()
+        {
+            using (var connection = new SqlConnection(Cnn))
+            {
+                connection.Open();               
+                return connection.Query<Racer>("select * from racer");
+            }
+        }
+    }
+
+    public class RoundController : ApiController
+    {
+        private const string Cnn = @"Server=.\sqlexpress;Database=Stumptown40;Trusted_Connection=True;";
+
+        public IEnumerable<int> Get()
+        {
+            using (var connection = new SqlConnection(Cnn))
+            {
+                connection.Open();
+                return connection.Query<int>("select * from round");
+            }
+        }
+    }
+
+    public class MatchController : ApiController
+    {
+        private const string Cnn = @"Server=.\sqlexpress;Database=Stumptown40;Trusted_Connection=True;";
+
+        public IEnumerable<Match> Get()
+        {
+            using (var connection = new SqlConnection(Cnn))
+            {
+                connection.Open();
+                return connection.Query<Match>("select * from match");
+            }
+        }
+
+        public IEnumerable<Match> GetMatchByRound(int roundId)
+        {
+            using (var connection = new SqlConnection(Cnn))
+            {
+                connection.Open();
+                return connection.Query<Match>("select * from match where roundid=@roundId", new {roundId});
+            }
+        }
+
+        // http://localhost:31464/api/match?matchId=1&winningracerid=2
+
+        public HttpResponseMessage Put(int matchId, int winningracerid)
+        {
+            using (var connection = new SqlConnection(Cnn))
+            {
+                connection.Open();
+
+                connection.Execute("update match set winningracerid=@winningracerid where matchid=@matchId", new { matchId, winningracerid });
+
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
         }
     }
 }
---
+++ v2013/src/Website/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Website.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public int RoundId { get; set; }
+        public int RaceNumber { get; set; }
+        public int Racer1Id { get; set; }
+        public int Racer2Id { get; set; }
+        public int WinningRacerId { get; set; }
+        public int NextWinningMatchId { get; set; }
+        public int NextWinningMatchSlot { get; set; }
+        public DateTime? Modified { get; set; }
+    }
+}
