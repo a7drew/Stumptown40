7f279f0f0970217dec76db913a1ccb3cf351c1cc
Ryan <parrfolio@gmail.com>
2013-04-25 00:01:29 -0700

    added winner, current, prevous

--- v2013/src/Website/Scripts/app.js
+++ v2013/src/Website/Scripts/app.js
@@ -8,18 +8,54 @@ chat.client.onNavigate = function (viewName, jsonData) {
 		this.bind('#'+viewName, function(event, viewName) {
 	      	var context = this;
 			var obj = jQuery.parseJSON(jsonData);
-
-			$.getJSON('http://stumptown40.cloudapp.net/api/racers?callback=?', function (e) {
-                gRacerCache = e;
-
-				//console.log((obj.currentRace[0].racer1) - 1)
-				context.racer1 = gRacerCache[(obj.currentRace[0].racer1) - 1].Name;
-				context.racer2 = gRacerCache[(obj.currentRace[0].racer2) - 1].Name;
+			var location = window.location.hash;
+			
+			context.title = jsonData;
+			
+			var raceView = function() {
+				$.getJSON('http://stumptown40.cloudapp.net/api/racers?callback=?', function (e) {
+	                gRacerCache = e;
+	
+					context.racer1 = gRacerCache[(obj.currentRace[0].racer1) - 1].Name;
+					context.racer2 = gRacerCache[(obj.currentRace[0].racer2) - 1].Name;
+					
+					context.racer1Id = obj.currentRace[0].racer1;
+					context.racer2Id = obj.currentRace[0].racer2;
+					
+					context.nextRacer1 = gRacerCache[(obj.upcomingRace[0].nextRacer1) - 1].Name;
+					context.nextRacer2 = gRacerCache[(obj.upcomingRace[0].nextRacer2) - 1].Name;
+					
+					context.winnerId = obj.winnerId;
+					
+					context.winner = '';
+					
+					context.render('../Templates/'+viewName+'.hb', viewName, function(html){					
+			        	context.$element().html(html);
+			      	}).then(function() { 
 				
+
+						//winner
+				       	if(context.racer1Id === context.winnerId) {
+							$('p[data-id="'+context.racer1Id+'"]').addClass("winner");
+
+						} else if(context.racer2Id === context.winnerId) {
+							$('p[data-id="'+context.racer2Id+'"]').addClass("winner");
+						} else {
+							$('p[data-id="'+context.racer1Id+'"], p[data-id="'+context.racer2Id+'"]').removeClass("winner");
+						}
+					});
+					
+	            });
+			}
+			
+			if(location === "#raceView") {
+				raceView();
+			} else {
 				context.render('../Templates/'+viewName+'.hb', viewName, function(html){					
 		        	context.$element().html(html);
-		      	});
-            });
+		      	});	
+			}
+			
 		});
 
 		this.get('', function(context) {
--- v2013/src/Website/Templates/raceView.hb
+++ v2013/src/Website/Templates/raceView.hb
@@ -1,2 +1,13 @@
-<p>Racer #{{racer1}}</p>
-<p>Racer #{{racer2}}</p>
+<div class="racerView">
+	<div>
+		<h1>Current Race</h1>
+		<p class="{{winner}}" data-id="{{racer1Id}}">Racer #{{racer1}}</p>
+		<p class="{{winner}}" data-id="{{racer2Id}}">Racer #{{racer2}}</p>
+	</div>
+
+	<div>
+		<h1>Upcoming Race</h1>
+		<p>Racer #{{nextRacer1}}</p>
+		<p>Racer #{{nextRacer2}}</p>
+	</div>
+</div>
