ec196f24cc16b00128b5eaeb8b46aa467d98605f
Ryan <parrfolio@gmail.com>
2014-05-03 22:54:38 -0700

    sponsor logos in, new homepage logo, two new templates for standard and unlimited divisions, gallery update with postano

--- v2014/src/Website/Scripts/app.js
+++ v2014/src/Website/Scripts/app.js
@@ -97,144 +97,7 @@ $('.racer[data-id="'+context.racer1Id+'"],.racer[data-id="'+context.racer2Id+'"]
 					
 				context.render('../Templates/'+viewName+'.hb', jsonData, function(html){					
 		        	context.$element().empty().html(html);
-		      	}).then(function() {
-			
-							var timeoutId = window.setTimeout(function() {}, 0);
-							while (timeoutId--) {
-							    window.clearTimeout(timeoutId); 
-							}
-
-
-							var intervalId = window.setInterval(function() {}, 0);
-							while (intervalId--) {
-							    window.clearInterval(intervalId);
-							}
-						 var getNewphoto = function() {
-							$.ajax({
-					          	type: "GET",
-					          	dataType: "jsonp",
-								jsonpCallback:"jsonp",
-					          	async: true,
-					          	cache: false,
-					          	url: s40,
-					          	success: function (data) {
-									var newphoto = data.posts[0];
-									
-									newphoto.newtext = (newphoto.text != null) ? newphoto.text : '';
-									newphoto.number = (newphoto.newtext.match(noletter) != null) ? newphoto.newtext : 'newphoto.newtext';
-														
-									var newphotoId = newphoto.post_id;
-									var prevphotoId = (!isNaN(parseInt($('#photos li:nth-child(1)').attr('data-id')))) ? parseInt($('#photos li:nth-child(1)').attr('data-id')) : newphotoId;
-									//console.log(newphotoId,prevphotoId)
-									if (newphotoId !== prevphotoId) {
-										context.render('../Templates/photos.hb', newphoto, function(e){
-											result = e;
-											return
-										}).then(function(result) {
-											$("#photos").prepend(result);
-										});	
-									}
-								}
-							});
-						}
-						var  l = 0;
-						var myList = [];
-					   $.ajax({
-				          	dataType: "jsonp",
-							jsonpCallback:"jsonp",
-				          	url: s40,
-				          	success: function (data) {
-								function shuffle(o){ //v1.0
-								for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-								return o;
-								};
-
-								var myArray = data.posts;
-								newArray = shuffle(myArray);	
-								l = newArray.length;
-								for (x = 0; x < l; x++) {
-									var photos = newArray[x];
-									photos.newtext = (photos.text != null) ? photos.text : '';
-									photos.number = photos.text;
-									
-									context.render('../Templates/photos.hb', photos).prependTo($("#photos"));
-								}
-							},
-						complete: function(){
-								var min = 1;
-								var timer = 6000;
-								var highlight = function() {
-									var max = $("#photos li").size();
-									var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-									$("#photos li").removeClass("on");
-									$("#photos").find("li:nth-child("+randomnumber+")").addClass("on");
-									setTimeout(function () {
-										window.requestAnimationFrame(highlight);
-						            }, timer);
-								}
-								
-								var highlight2 = function() {
-									var max = $("#photos li").size();
-									var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-									$("#photos li").removeClass("on2");
-									$("#photos").find("li:nth-child("+randomnumber+")").addClass("on2");
-									setTimeout(function () {
-										window.requestAnimationFrame(highlight2);
-						            }, 1000);
-								}
-								
-								var highlight3 = function() {
-									var max = $("#photos li").size();
-									var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-									$("#photos li").removeClass("on3");
-									$("#photos").find("li:nth-child("+randomnumber+")").addClass("on3");
-									setTimeout(function () {
-										window.requestAnimationFrame(highlight3);
-						            }, 1200);
-								}
-								
-								var highlight4 = function() {
-									var max = $("#photos li").size();
-									var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-									$("#photos li").removeClass("on4");
-									$("#photos").find("li:nth-child("+randomnumber+")").addClass("on4");
-									setTimeout(function () {
-										window.requestAnimationFrame(highlight4);
-						            }, 1500);
-								}
-								
-								var highlight5 = function() {
-									var max = $("#photos li").size();
-									var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
-									$("#photos li").removeClass("on5");
-									$("#photos").find("li:nth-child("+randomnumber+")").addClass("on5");
-									setTimeout(function () {
-										window.requestAnimationFrame(highlight5);
-						            }, 2000);
-								}
-								
-								window.requestAnimationFrame(highlight);
-								window.requestAnimationFrame(highlight2);
-								window.requestAnimationFrame(highlight3);
-								window.requestAnimationFrame(highlight4);
-								window.requestAnimationFrame(highlight5);
-								
-								(function poll() {
-						            if (window.location.hash != '#gallery')
-						                return;
-
-						            setTimeout(function ()
-						            {
-						                getNewphoto();
-						                poll();
-						            }, 5000);
-						       })();
-						
-						
-						}
-					});
-			
-				});
+		      	});
 			}
 			
 			if(location === "#raceView") {				
@@ -280,7 +143,7 @@ $.connection.hub.start({
     jsonp: isChrome
 }).done(function () {
 	
-/* for Ryan to debug quickly	
+/*for Ryan to debug quickly	*/
 if(window.location.hash == "#home") {
 	chat.client.onNavigate("home", '');
 }	
@@ -295,7 +158,7 @@ chat.client.onNavigate("sponsors", '');
 if(window.location.hash == "#gallery") {
 	chat.client.onNavigate("gallery", '');
 }	
-*/
+
 
 
 });
